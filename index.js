9073b14d3fdc5b93a2f69e4d9085fe85aa3cefed
KeziahOkyere41 <Nharnhayhaar208@gmail.com>
2022-06-24 10:53:01 +0000

    completed lab

--- index.js
+++ index.js
@@ -1,9 +1,35 @@
+function reverseString(word) {
+  // create an array from the input string
+  const stringReverse = word.split("");
+
+    // reverse the array
+  const reverseArray = stringReverse.reverse();
+
+    // create a string from the reversed array
+  const reversedWordArray = reverseArray.join("");
+  
+  return reversedWordArray;
+}
+
 function isPalindrome(word) {
-  // Write your algorithm here
+  // reverse the input string
+  const reversedWord = reverseString(word);
+  // if the reversed string is the same as the input
+  if (word === reversedWord) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 /* 
   Add your pseudocode here
+  reverse the input string
+
+if the reversed string is the same as the input
+  return true
+else
+  return false
 */
 
 /*
